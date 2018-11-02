db3a151280a6569fd3e1718cb040bc60c612c7a8
Leo Generali <leogen17@gmail.com>
2018-11-02 13:24:32 -0400

    Add resumes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
-  root to: 'home#index'
+
+  scope 'resumes' do
+    root 'resumes#index', as: 'resume_page_root'
+    get '/editor', to: 'resumes#create', as: 'resume_editor'
+  end
+
+  root to: 'home#index', as: 'application_root'
 end
