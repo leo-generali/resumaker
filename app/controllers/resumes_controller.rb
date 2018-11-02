f473e2b26a72558690f435a1e6a49863f4570e4b
Leo Generali <leogen17@gmail.com>
2018-11-02 15:27:44 -0400

    Add ability to create resumes when signed in

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,7 +1,26 @@
 class ResumesController < ApplicationController
   def index
+    @resume = Resume.new
+    if current_user.nil?
+      @resumes = []
+    else
+      @resumes = Resume.where(user_id:current_user.id)
+    end
   end
 
   def create
+    if current_user.nil?
+      redirect_to resume_editor_path
+    else
+      @resume = current_user.resumes.new()
+      if @resume.save
+        redirect_to resume_editor_path, notice: 'Succesfully created a new resume!'
+      else
+        redirect_to application_root_path, notice: 'Did not create new resume!'
+      end
+    end
+  end
+
+  def edit
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+
+  # User Registration
+  # devise_scope :user do
+  #   get '/login', to: 'devise/sessions#new', as: 'new_user_session' # custom path to login/sign_in
+  #   get '/register', to: 'devise/registrations#new', as: 'new_user_registration' # custom path to sign_up/registration
+  # end
+
   devise_for :users, controllers: { registrations: 'registrations' }
 
   scope 'resumes' do
     root 'resumes#index', as: 'resume_page_root'
-    get '/editor', to: 'resumes#create', as: 'resume_editor'
+    post '/new', to: 'resumes#create', as: 'resume_create'
+    get '/editor', to: 'resumes#edit', as: 'resume_editor'
   end
 
   root to: 'home#index', as: 'application_root'
