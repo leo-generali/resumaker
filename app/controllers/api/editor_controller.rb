31e90806650781d7e1e7f3d7b2e59580d3f0fb1f
Leo Generali <leogen17@gmail.com>
2018-11-04 17:15:10 -0500

    Add links to resumes

--- app/controllers/api/editor_controller.rb
+++ app/controllers/api/editor_controller.rb
@@ -1,7 +1,6 @@
 class Api::EditorController < ApplicationController
   def show
     @resume = Resume.find(params[:id])
-    # render json: @resume, :in
     render json: @resume, include: [:basic_info], except: [:created_at, :updated]
   end
 end
