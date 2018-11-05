b7b6a37284dd30a5dde0667cb72bba8f85c878b4
Leo Generali <leogen17@gmail.com>
2018-11-04 19:52:13 -0500

    Add education info on create

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -3,16 +3,16 @@ class Resume < ApplicationRecord
   has_one :basic_info, dependent: :destroy
   has_many :education_infos, dependent: :destroy
 
-  after_create :create_basic_info
+  after_create :create_basic_info, :create_education_info
 
   private
     def create_basic_info
-      basic_info = BasicInfo.create()
+      basic_info = BasicInfo.create(name: 'leo')
       self.basic_info = basic_info
     end
 
-    def education_info
-      education_info = EducationInfo.create()
-      self.education_info = education_info
+    def create_education_info
+      education_info = EducationInfo.create(institution: 'leo school')
+      self.education_infos << education_info
     end
 end
